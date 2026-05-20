2c907e879ec433f779d4d2b2ea502f615ace65c2
Edwin Uriel Astudillo Pérez <eastudillop2000@alumno.ipn.mx>
2026-05-20 17:52:08 -0600

    build: add tanstack suite, nuqs dependencies and configure providers

--- apps/web/src/providers/index.tsx
+++ apps/web/src/providers/index.tsx
@@ -1,8 +1,31 @@
 "use client";
 
 import { Toaster } from "@juris-os/ui/components/sonner";
-
+import dynamic from "next/dynamic";
+import { NuqsProvider } from "./nuqs";
+import { ReactQueryProvider } from "./react-query";
 import { ThemeProvider } from "./theme-provider";
+
+const TanStackGlobalDevtools =
+	process.env.NODE_ENV === "production"
+		? () => null
+		: dynamic(
+				() =>
+					Promise.all([
+						import("@tanstack/react-devtools"),
+						import("@tanstack/react-form-devtools"),
+					]).then(([devtoolsMod, formPluginMod]) => {
+						return function DevtoolsWrapper() {
+							return (
+								<devtoolsMod.TanStackDevtools
+									plugins={[formPluginMod.formDevtoolsPlugin()]}
+									eventBusConfig={{ debug: true }}
+								/>
+							);
+						};
+					}),
+				{ ssr: false },
+			);
 
 export function Providers({ children }: { children: React.ReactNode }) {
 	return (
@@ -12,8 +35,13 @@ export function Providers({ children }: { children: React.ReactNode }) {
 			enableSystem={false}
 			disableTransitionOnChange
 		>
-			{children}
-			<Toaster richColors />
+			<ReactQueryProvider>
+				<NuqsProvider>{children}</NuqsProvider>
+
+				<Toaster position="top-center" theme="light" richColors />
+
+				<TanStackGlobalDevtools />
+			</ReactQueryProvider>
 		</ThemeProvider>
 	);
 }

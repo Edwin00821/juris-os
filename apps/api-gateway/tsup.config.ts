46c475cbfd6cc542bbcc68c88504146aec174fa1
Edwin Uriel Astudillo Pérez <eastudillop2000@alumno.ipn.mx>
2026-05-22 00:18:55 -0600

    fix: disable code splitting to generate single bundle for Vercel

--- apps/api-gateway/tsup.config.ts
+++ apps/api-gateway/tsup.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-	entry: ["api/index.ts"],
+	entry: ["./api/index.ts"],
 	format: ["esm"],
 	outDir: "dist",
 	clean: true,
+	bundle: true,
+	splitting: false,
 	noExternal: [/@juris-os\/.*/],
 });
